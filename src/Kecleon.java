90dbdbd7dbe97b393aeabee13a0da364580a35dc
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-09-27 11:15:55 +0300

    urpokeconstrvar1+name

--- src/Kecleon.java
+++ src/Kecleon.java
@@ -1,19 +1,23 @@
 import ru.ifmo.se.pokemon.Pokemon;
 
 public class Kecleon extends Pokemon {
-int HP;
-int ATTACK;
-int DEFENSE;
-int SPEED;
-int SPECIAL_ATTACK;
-int SPECIAL_DEFENSE;
-    public Kecleon (int HP, int ATTACK, int DEFENSE, int SPEED, int SPECIAL_ATTACK, int SPECIAL_DEFENSE){
-this.HP = HP;
-this.ATTACK = ATTACK;
-this.DEFENSE = DEFENSE;
-this.SPEED = SPEED;
-this.SPECIAL_ATTACK = SPECIAL_ATTACK;
-this.SPECIAL_DEFENSE = SPECIAL_DEFENSE;
+    String name;
+    int lvl;
+    int HP;
+    int ATTACK;
+    int DEFENSE;
+    int SPEED;
+    int SPECIAL_ATTACK;
+    int SPECIAL_DEFENSE;
+    public Kecleon (String name, int lvl, int HP, int ATTACK, int DEFENSE, int SPEED, int SPECIAL_ATTACK, int SPECIAL_DEFENSE){
+        this.name = name;
+        this.lvl = lvl;
+        this.HP = HP;
+        this.ATTACK = ATTACK;
+        this.DEFENSE = DEFENSE;
+        this.SPEED = SPEED;
+        this.SPECIAL_ATTACK = SPECIAL_ATTACK;
+        this.SPECIAL_DEFENSE = SPECIAL_DEFENSE;
     }
 }
    // int HP, int ATTACK, int DEFENSE, int SPEED, int SPECIAL_ATTACK, int SPECIAL_DEFENSE
