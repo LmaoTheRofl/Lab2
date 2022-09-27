da00d641ec80737bc2678a34e50f9639cb887431
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-09-27 13:45:53 +0300

    nextmove(setSTATS)BUGMB

--- src/Kecleon.java
+++ src/Kecleon.java
@@ -5,16 +5,11 @@ public class Kecleon extends Pokemon {
     String name;
     int level;
     double[] base;
-    public Kecleon (String name, int level, double HP, double Attack, double Defense, double specattack, double specdefense, double speed) {
+    public Kecleon (String name, int level) {
         this.base = new double[Stat.values().length];
         this.name = name;
         this.level = level;
-        this.base[Stat.HP.ordinal()] = HP;
-        this.base[Stat.ATTACK.ordinal()] = Attack;
-        this.base[Stat.DEFENSE.ordinal()] = Defense;
-        this.base[Stat.SPECIAL_ATTACK.ordinal()] = specattack;
-        this.base[Stat.SPECIAL_DEFENSE.ordinal()] = specdefense;
-        this.base[Stat.SPEED.ordinal()] = speed;
+
     }
 }
    // int HP, int ATTACK, int DEFENSE, int SPEED, int SPECIAL_ATTACK, int SPECIAL_DEFENSE
