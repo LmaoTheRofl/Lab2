1e950f05580dbc9001f165468bc7635ad45e96d0
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-09-27 13:45:53 +0300

    nextmove

--- src/Kecleon.java
+++ src/Kecleon.java
@@ -5,10 +5,16 @@ public class Kecleon extends Pokemon {
     String name;
     int level;
     double[] base;
-    public Kecleon (String name, int level) {
+    public Kecleon (String name, int level, double HP, double Attack, double Defense, double specattack, double specdefense, double speed) {
         this.base = new double[Stat.values().length];
         this.name = name;
         this.level = level;
+        this.base[Stat.HP.ordinal()] = HP;
+        this.base[Stat.ATTACK.ordinal()] = Attack;
+        this.base[Stat.DEFENSE.ordinal()] = Defense;
+        this.base[Stat.SPECIAL_ATTACK.ordinal()] = specattack;
+        this.base[Stat.SPECIAL_DEFENSE.ordinal()] = specdefense;
+        this.base[Stat.SPEED.ordinal()] = speed;
     }
 }
    // int HP, int ATTACK, int DEFENSE, int SPEED, int SPECIAL_ATTACK, int SPECIAL_DEFENSE
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
 
 public class Main {
     public static void main(String[] args) {
 
     Battle b = new Battle();
-    Pokemon p1 = new Pokemon("Чужой", 1);
+    Kecleon p1 = new Kecleon("Чужой", 1, 60.0D, 90, 70, 60, 120, 40);
     Pokemon p2 = new Pokemon("Хищник", 1);
+        System.out.println(p1.getStat(Stat.HP));
 b.addAlly(p1);
 b.addFoe(p2);
 b.go();
