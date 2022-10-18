062d94be54fb7c5a8a0f9729438889c31f2c2a81
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-10-18 23:08:54 +0300

    Lab2rename*

--- src/Lab2.java
+++ src/Lab2.java
@@ -2,7 +2,7 @@ import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Stat;
 
-public class Main {
+public class Lab2 {
     public static void main(String[] args) {
 
     Battle b = new Battle();
