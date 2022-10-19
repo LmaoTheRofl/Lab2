49f6e4d6d7bc538b571d982bd451a70db73c8dc0
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-10-19 10:20:02 +0300

    bcreateteam*

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,16 +1,24 @@
 import ru.ifmo.se.pokemon.Battle;
-import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Stat;
 
 public class Lab2 {
     public static void main(String[] args) {
 
     Battle b = new Battle();
-    Heatran p1 = new Heatran("Чужой", 1);
-    Pokemon p2 = new Pokemon("Хищник", 1);
+    Heatran p1 = new Heatran("Гном", 1);
+    Tyrogue p2 = new Tyrogue("Никитка", 1);
+    Hitmontop p3 = new Hitmontop("Гоша", 1);
+    Poliwag p4 = new Poliwag("Семён", 1);
+    Poliwhirl p5 = new Poliwhirl("Кархтавый", 1);
+    Politoed p6 = new Politoed("Антон", 1);
+
         System.out.println(p1.getStat(Stat.HP));
 b.addAlly(p1);
+b.addAlly(p3);
+b.addAlly(p5);
 b.addFoe(p2);
+b.addFoe(p4);
+b.addFoe(p6);
 b.go();
 
 
